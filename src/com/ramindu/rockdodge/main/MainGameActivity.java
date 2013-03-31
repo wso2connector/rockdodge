2710e7adedabbf5a25a1dcb39398d2543f233110
Ramindu Deshapriya <rasade88@gmail.com>
2013-03-31 20:34:30 +0530

    Added initial code with Augmented Reality Background and moving sprite

---
+++ src/com/ramindu/rockdodge/main/MainGameActivity.java
@@ -0,0 +1,92 @@
+package com.ramindu.rockdodge.main;
+
+import java.io.IOException;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.ConfigChooserOptions;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.augmentedreality.SimpleBaseAugmentedRealityGameActivity;
+import org.andengine.opengl.texture.ITexture;
+import org.andengine.opengl.texture.bitmap.AssetBitmapTexture;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TextureRegionFactory;
+import org.andengine.util.adt.color.Color;
+
+import android.view.Display;
+import android.widget.Toast;
+
+public class MainGameActivity extends SimpleBaseAugmentedRealityGameActivity {
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+	
+	private float cameraWidth;
+	private float cameraHeight;
+
+	private ITexture mRockTexture;
+	private ITextureRegion mRockTextureRegion;
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		final Display display = getWindowManager().getDefaultDisplay();
+		cameraWidth = display.getWidth();
+		cameraHeight = display.getHeight();
+
+		final Camera camera = new Camera(0, 0, cameraWidth, cameraHeight);
+
+		final EngineOptions engineOptions = new EngineOptions(true,
+				ScreenOrientation.LANDSCAPE_SENSOR, new RatioResolutionPolicy(
+						cameraWidth, cameraHeight), camera);
+		final ConfigChooserOptions configChooserOptions = engineOptions
+				.getRenderOptions().getConfigChooserOptions();
+		configChooserOptions.setRequestedRedSize(8);
+		configChooserOptions.setRequestedGreenSize(8);
+		configChooserOptions.setRequestedBlueSize(8);
+		configChooserOptions.setRequestedAlphaSize(8);
+		configChooserOptions.setRequestedDepthSize(16);
+		return engineOptions;
+	}
+
+	@Override
+	public void onCreateResources() throws IOException {
+		mRockTexture = new AssetBitmapTexture(getTextureManager(), getAssets(),
+				"gfx/rock.png");
+		mRockTextureRegion = TextureRegionFactory.extractFromTexture(mRockTexture);
+		mRockTexture.load();
+	}
+
+	@Override
+	public Scene onCreateScene() {
+		mEngine.registerUpdateHandler(new FPSLogger());
+
+		Scene scene = new Scene();
+		scene.getBackground().setColor(Color.TRANSPARENT);
+
+		final float centerX = cameraWidth / 2;
+		final float centerY = cameraHeight / 2;
+
+		final Sprite rock = new Sprite(centerX, centerY,
+				mRockTextureRegion, this.getVertexBufferObjectManager());
+		rock.registerEntityModifier(new MoveModifier(30, 0, 0, cameraWidth,
+				cameraHeight));
+		rock.setScale(0.5f);
+		scene.attachChild(rock);
+
+		return scene;
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
